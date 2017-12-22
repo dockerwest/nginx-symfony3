d43c06c42bc219032f8a8c1c0e8e43a7a734560f
Tom Van Herreweghe <tom.van.herreweghe@dx-solutions.be>
2017-12-22 13:45:43 +0100

    Build Symfony3 image twice for BC reasons
    Signed-off-by: Tom Van Herreweghe <tom.van.herreweghe@dx-solutions.be>

--- 3/mainline/build.sh
+++ 3/mainline/build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 docker pull nginx:mainline-alpine
 
+docker build --no-cache -t dockerwest/nginx-symfony:mainline .
 docker build --no-cache -t dockerwest/nginx-symfony-3:mainline .
--- 3/stable/build.sh
+++ 3/stable/build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 docker pull nginx:stable-alpine
 
+docker build --no-cache -t dockerwest/nginx-symfony:stable .
 docker build --no-cache -t dockerwest/nginx-symfony-3:stable .
