50fa761261544a9dc4a0a84f3a342ca8d1ccf52c
Tom Van Herreweghe <tom.van.herreweghe@dx-solutions.be>
2017-12-22 13:57:45 +0100

    Keep same docker image name, set symfony version in the tag
    Signed-off-by: Tom Van Herreweghe <tom.van.herreweghe@dx-solutions.be>

--- 4/mainline/build.sh
+++ 4/mainline/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 docker pull nginx:mainline-alpine
 
-docker build --no-cache -t dockerwest/nginx-symfony-4:mainline .
+docker build --no-cache -t dockerwest/nginx-symfony:4-mainline .
--- 4/stable/build.sh
+++ 4/stable/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 docker pull nginx:stable-alpine
 
-docker build --no-cache -t dockerwest/nginx-symfony-4:stable .
+docker build --no-cache -t dockerwest/nginx-symfony:4-stable .
